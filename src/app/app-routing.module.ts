cc61635a0ff8eb1b24d911e72163736e349abb00
Darlan de Castro <darlandc@gmail.com>
2019-12-11 11:12:27 -0300

    fix child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePage } from './home/home.page';
+import { AboutPage } from './about/about.page';
+import { ChildPage } from './child/child.page';
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage
+  },
+  {
+    path: 'about',
+    component: AboutPage,
+    children: [
+      {
+        path: 'child',
+        component: ChildPage
+      }
+    ]
+  }
 
 ];
 
